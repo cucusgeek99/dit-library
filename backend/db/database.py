752eaec9a2d1f6dc73db0936900263e7c47259ba
DonBos27 <bosengad@gmail.com>
2026-03-24 23:26:08 +0100

    feat: configuration MySQL et migration Alembic (port corrige a 3306)

--- backend/db/database.py
+++ backend/db/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://admin:admin123@localhost:33060/dit-library-bd"
+SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://admin:admin123@localhost:3306/dit-library-bd"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 session_local = sessionmaker(autocommit=False, autoflush=False, bind=engine)
